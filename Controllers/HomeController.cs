0bb5a2db948334704efd2769f6c8ecb1349c73c3
JoshieBoy454 <Joshuagreggain@gmail.com>
2024-10-17 23:40:30 +0200

    can successfully store the data input and display it

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 using Contract_Monthly_Claim_System.Models2;
 using Contract_Monthly_Claim_System2.Models;
+using Elfie.Serialization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Security.Claims;
 
 namespace Contract_Monthly_Claim_System2.Controllers
 {
@@ -33,10 +36,23 @@ namespace Contract_Monthly_Claim_System2.Controllers
         }
         public IActionResult ClaimForm(CreateClaim model)
         {
+            if (model.DocumentFile != null && model.DocumentFile.Length > 0)
+            {
+                var fileName = Path.GetFileName(model.DocumentFile.FileName);
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads", model.DocumentFile.FileName);
+
+                Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads"));
+
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.DocumentFile.CopyTo(fileStream);
+                }
+                model.Document = $"/uploads/{fileName}";
+            }
             _context.Claims.Add(model);
             _context.SaveChanges();
 
-            return RedirectToAction("Claim");// to change to a page that will display the claims submitted by specific lecturer
+            return RedirectToAction("ClaimHub");
         }
         public IActionResult ClaimHub()
         {
--- Models/CreateClaim.cs
+++ Models/CreateClaim.cs
@@ -1,13 +1,18 @@
-﻿namespace Contract_Monthly_Claim_System.Models2
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Contract_Monthly_Claim_System.Models2
 {
     public class CreateClaim
     {
+        public int ID { get; set; }
         public string? Name { get; set; }
         public string? Surname { get; set; }
         public double Hours { get; set; }
         public double Rate { get; set; }
         public string? Notes { get; set; }
-        public IFormFile? Document { get; set; }
+        [NotMapped]
+        public IFormFile? DocumentFile { get; set; }
+        public string? Document { get; set; }
         public int Approval { get; set; }
     }
 }
